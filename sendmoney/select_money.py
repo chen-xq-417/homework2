43cec35e839e862f440dcdc462a348d3343d9eac
chen-xq-417 <1322554187@qq.com>
2021-01-31 18:55:31 +0800

    上传【发工资】代码

---
+++ sendmoney/select_money.py
@@ -0,0 +1,6 @@
+# 定义工资查询模块，用来展示工资数额
+import homework.sendmoney.send_money
+
+
+def select_money():
+    print(homework.sendmoney.send_money.send_money())
---
+++ sendmoney/send_money.py
@@ -0,0 +1,7 @@
+from homework.sendmoney import money
+
+
+# 定义发工资模块，用来增加收入计算
+def send_money():
+    money.saved_money += 1000
+    return money.saved_money
